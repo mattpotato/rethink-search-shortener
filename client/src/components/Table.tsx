4a5b7dce87b9642127235f0f5331b06a17fcd445
matt011 <38515217+matt011@users.noreply.github.com>
2020-11-24 23:31:59 +0800

    create table component

---
+++ client/src/components/Table.tsx
@@ -0,0 +1,67 @@
+import { useMemo } from "react";
+import DataTable from "react-data-table-component";
+import { PeopleConnection } from "../generated/graphql";
+
+interface TableProps {
+  data: PeopleConnection | undefined | null;
+  loadMore: Function;
+}
+
+const Table: React.FC<TableProps> = ({ data, loadMore }) => {
+  const columns = useMemo(
+    () => [
+      {
+        name: "First Name",
+        selector: "firstName",
+        sortable: true,
+      },
+      {
+        name: "Last Name",
+        selector: "lastName",
+        sortable: true,
+      },
+      {
+        name: "Job Title",
+        selector: "jobTitle",
+        sortable: true,
+      },
+      {
+        name: "Country",
+        selector: "country",
+        sortable: true,
+      },
+      {
+        name: "Email",
+        selector: "email",
+        sortable: true,
+      },
+    ],
+    []
+  );
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <DataTable
+        title="People"
+        columns={columns}
+        data={data?.nodes ? data.nodes : []}
+        paginationTotalRows={data?.totalCount}
+        style={{ marginBottom: 20 }}
+      />
+      <button
+        onClick={() => loadMore()}
+        style={{ display: data?.pageInfo.hasNextPage ? undefined : "none" }}
+      >
+        load more
+      </button>
+    </div>
+  );
+};
+
+export default Table;
