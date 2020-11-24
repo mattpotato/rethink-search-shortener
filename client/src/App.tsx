fbeb581a5737c1363d44fedaf0e5d797d42a20b8
matt011 <38515217+matt011@users.noreply.github.com>
2020-11-24 19:03:14 +0800

    setup apollo client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { ApolloProvider } from "@apollo/client";
+
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  return <div>app</div>;
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+      </div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
