7e04ed298cc698b491a3a22892cc6dc23802843d
matt011 <38515217+matt011@users.noreply.github.com>
2020-11-25 17:10:20 +0800

    create styled components

---
+++ client/src/components/StyledButton.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  height: 30px;
+  border-radius: 3px;
+  color: white;
+  background: #3182ce;
+  font-size: 20px;
+  border: 0px;
+`;
---
+++ client/src/components/StyledInput.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledInput = styled.input`
+  width: 500px;
+  border-radius: 10px;
+  padding: 5px;
+  font-size: 14px;
+  border: 1px solid #dfe1e5;
+  :focus {
+    outline: none;
+  }
+`;
