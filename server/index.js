4b6b833ae32ca149541229e68bea4ae9b09f6d5d
matt011 <38515217+matt011@users.noreply.github.com>
2020-11-25 17:09:42 +0800

    add url redirection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const { postgraphile } = require("postgraphile");
-
+const Pool = require("pg").Pool;
 const app = express();
+
+const pool = new Pool({
+  connectionString: "postgres://postgres:postgres@localhost:5432/dbsearch",
+});
+
+app.get("/s/:shortId", (req, res) => {
+  const shortId = req.params.shortId;
+  pool.query(
+    `select * from url where short_id='${shortId}'`,
+    (error, results) => {
+      if (error) {
+        res.status(404).json({ error: 404 });
+      }
+      const url = results.rows[0];
+      if (url) {
+        res.redirect(url.long_url);
+        return;
+      }
+      res.status(404).json({ error: "url does not exist" });
+    }
+  );
+});
 
 app.use(
   postgraphile(
